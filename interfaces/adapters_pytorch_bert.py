0e45c5ec5aa120e73bec5184da1cab85ff4b50fc
GradyKurpasi <45749138+GradyKurpasi@users.noreply.github.com>
2021-10-28 19:50:27 -0700

    Bert Helpers Done

--- interfaces/adapters_pytorch_bert.py
+++ interfaces/adapters_pytorch_bert.py
@@ -20,7 +20,7 @@ def print_inputs(input_text, inputs):
     print(f'Length of BERT base vocabulary: {len(tokenizer.vocab)}')
     sent_count = 1
     for tensors in inputs['input_ids']:
-        print('\nSentence: ' + str(sent_count))
+        print('\nSequence: ' + str(sent_count))
         print('Tokens: ' + str(len(tensors)))
         word_count = 0
         for tokens in tensors:
@@ -112,19 +112,15 @@ def print_outputs(outputs):
     #   dims of tensors = batchsize x sequence length x embedding size (e.g. 1 x 5 x 768)
     print('\nHidden States tensor shape: ' + str(outputs['hidden_states'][0].shape))
 
-
     # mean of all sequence embeddings (i.e. entire sentence), shape = 1 x 768
     seq_mean = outputs['last_hidden_state'].mean(1)
     print("\nMean of all word embeddings for sentence 1:")
     # print(seq_mean)
 
-
     print(outputs['hidden_states'])
     print("\n\n")
     tgt_batch = 0
     tgt_word = 1
-
-
 
     first_layer = first_hidden_layer(outputs['hidden_states'], tgt_word, tgt_batch)
     # assert torch.all(torch.eq(outputs['hidden_states'][0][0][1], first_layer))
@@ -138,13 +134,7 @@ def print_outputs(outputs):
     concat_last4_layers = concat_last4_hidden_layers(outputs['hidden_states'], tgt_word, tgt_batch)
 
 
-
-
-
-
-
-
-def simple_model():
+def example_simple_model():
     # MODEL
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
     model = AutoModel.from_pretrained("bert-base-uncased")
@@ -163,84 +153,68 @@ def simple_model():
     print_outputs(outputs)
 
 
-
-
-def simple_model_multiple_inputs():
+def example_simple_model_multiple_inputs():
     # MODEL
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     model = BertModel.from_pretrained("bert-base-uncased")
 
     #INPUT
-    text = ['Hello There!', 'Goodbye for now.']
+    input_text = ['Hello There!', 'Goodbye for now.']
 
     # TOKENIZER
-    tokens = tokenizer(text, padding=True, truncation=True, return_tensors='pt')
-    print (tokens)
-    print(f'Length of BERT base vocabulary: {len(tokenizer.vocab)}')
-    print(f'Text: {text}')
-    for inputs in tokens['input_ids']:
-        for t in inputs:
-            print(f'Token: {t}, subword: {tokenizer.decode([t])}')
-    print(len(tokens['input_ids']))
-
-    # BATCHING 
-    # unsqueeze adds a singleton at specified dimension
-    # used to set batch size of input
-    # inputs = torch.tensor(tokens).unsqueeze(0) # Batch size 1
+    inputs = tokenizer(input_text, padding=True, truncation=True, return_tensors='pt')    # returns: {'input_ids' : [tensor], 'token_type_ids' : [tensor], 'attention_mask' : [tensor]}
+    print_inputs(input_text, inputs)
 
     # MODEL OUTPUTS
-    outputs = model(**tokens)
-    print(f'output type: {type(outputs)}, output length: {len(outputs)}')
-    # Output 1 dims = batch size x sequence length x 768 (embedding size or hidden size)
-    print(f'first item shape: {outputs[0].shape}')
-    # Output 2 dims = batchsize x 768 (pooled output - represents whole sequence / correspondes to [CLS] token)
-    print(f'second item shape: {outputs[1].shape}')
-    # mean of sequence embeddings = 1 x 768 (embedding size or hidden size)
-    seq_mean = outputs[0].mean(1)
-    print(outputs)
+    # outputs = model(**input)
+    outputs = model(**inputs, output_hidden_states=True)
+    print_outputs(outputs)
 
 
-
-
-def paired_inputs():
+def example_paired_inputs():
     # MODEL
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     model = BertModel.from_pretrained("bert-base-uncased")
 
+    input_text1 = "How old are you?"
+    input_text2 = "I'm 6 years old"
+
+
     # TOKENIZER
-    encoded_input = tokenizer("How old are you?", "I'm 6 years old")
-    print(encoded_input)
+    # inputs = tokenizer(input_text, padding=True, return_tensors='pt')
+    # inputs = tokenizer("How old are you?", "I'm 6 years old", return_tensors='pt')
+    inputs = tokenizer(input_text1, input_text2, return_tensors='pt')
+    print_inputs([input_text1,input_text2], inputs)
 
     # MODEL OUTPUTS
-    outputs = model(**encoded_input)
-    print(f'output type: {type(outputs)}, output length: {len(outputs)}')
-    # Output 1 dims = batch size x sequence length x 768 (embedding size or hidden size)
-    print(f'first item shape: {outputs[0].shape}')
-    # Output 2 dims = batchsize x 768 (pooled output - represents whole sequence / correspondes to [CLS] token)
-    print(f'second item shape: {outputs[1].shape}')
-    # mean of sequence embeddings = 1 x 768 (embedding size or hidden size)
-    # seq_mean = outputs[0].mean(1)
-    print(outputs)
+    outputs = model(**inputs, output_hidden_states=True)
+    print_outputs(outputs)
 
 
-def multiple_paired_inputs():
+def example_multiple_paired_inputs():
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-    batch_sentences = ["Hello I'm a single sentence",
+    model = BertModel.from_pretrained("bert-base-uncased")
+    
+    input_text1 = ["Hello I'm a single sentence",
                    "And another sentence",
                    "And the very very last one"]
-    batch_of_second_sentences = ["I'm a sentence that goes with the first sentence",
-                             "And I should be encoded with the second sentence",
-                             "And I go with the very last one"]
-    encoded_inputs = tokenizer(batch_sentences, batch_of_second_sentences)
-    print(encoded_inputs)
-    print(encoded_inputs['input_ids'][0])
-    for t in encoded_inputs['input_ids'][0]:
-        print(f'Token: {t}, subword: {tokenizer.decode([t])}')
-    print(len(encoded_inputs['input_ids'][0]))
-    print(encoded_inputs['token_type_ids'][0])
+    input_text2 = ["I'm a sentence that goes with the first sentence",
+                   "And I should be encoded with the second sentence",
+                   "And I go with the very last one"]
 
 
-def masked_language_model():
+    # TOKENIZER
+    # inputs = tokenizer(input_text, padding=True, return_tensors='pt')
+    # inputs = tokenizer("How old are you?", "I'm 6 years old", return_tensors='pt')
+    inputs = tokenizer(input_text1, input_text2, padding=True, truncation=True, return_tensors='pt')
+    print_inputs([input_text1,input_text2], inputs)
+
+    # MODEL OUTPUTS
+    outputs = model(**inputs, output_hidden_states=True)
+    print_outputs(outputs)
+
+
+def example_masked_language_model():
     # MASKED LANGUAGE MODEL EXAMPLE
     nlp = pipeline("fill-mask")
     preds = nlp(f"I am 'FUCKED', it's been a very {nlp.tokenizer.mask_token} day.")
@@ -248,8 +222,7 @@ def masked_language_model():
     for p in preds: print(nlp.tokenizer.decode([p['token']]))
 
 
-
-def next_sentence_prediction():
+def example_next_sentence_prediction():
     # NEXT SENTENCE PREDICTION EXAMPLE
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased')
@@ -274,26 +247,18 @@ def next_sentence_prediction():
     print(first_sentence + ' ' + second_sentence_wrong)
     print(f'Next sentence prediction: {wrong_seq_relationship_scores.detach().numpy().flatten()[0] > 0}')
 
-    print("DONE")
-
-
-
-    # MODEL
-    tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased")
-    model = AutoModel.from_pretrained("bert-large-uncased")
-    model.config.output_attentions = True
-
-
-
-# next_sentence_prediction()
-# masked_language_model()
 
 
 
 
 
-simple_model()
-# simple_model_multiple_inputs()
 
+
+
+
+# simple_model()
+# example_simple_model_multiple_inputs()
+
+example_multiple_paired_inputs()
 
 
