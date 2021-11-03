44dd4d4f1101419cbb948741972e875793e3a9d7
GradyKurpasi <45749138+GradyKurpasi@users.noreply.github.com>
2021-11-03 01:45:04 -0700

    Ready for key import

--- run_local.py
+++ run_local.py
@@ -1,8 +1,8 @@
 from  services.semeval2015_import import  *
 
-import_semeval2015_lemma()
+# import_semeval2015_lemma()
 
-
+import_semeval2015_keys()
 
 
 
--- services/semeval2015_import.py
+++ services/semeval2015_import.py
@@ -18,9 +18,10 @@ def open_semeval2015_data(filename=DATA_FILE):
 
 
 def open_semeval2015_keys(filename=KEY_FILE):
-    """returns tab delimited reader"""
-    with open(filename, newline = '') as file:                                                                                          
-        return csv.reader(file, delimiter='\t')
+    """returns tab delimited fileobject"""
+    # with open(filename, newline = '') as file:                                                                                          
+    file = open(filename)                                                                                           
+    return csv.reader(file, delimiter='\t')
 
 
 #############################################
@@ -36,7 +37,7 @@ def write_source(client, source_id, source_type, source_year, source_desc):
 
 def write_lemma(client, lemma, token, address, pos, source_id):
     query = ["g.addV('lemma').property('id', '{}').property('pk', '/pk')".format(lemma), 
-            "g.V().hasLabel('lemma').has('id', '{}').addE('occursIn').property('address', '{}').property('token', '{}').property('pos', '{}').to(g.V().hasLabel('source').has('id', '{}'))".format(
+            "g.V().hasLabel('lemma').has('id', '{}').addE('occursIn').property('id', '{}').property('token', '{}').property('pos', '{}').to(g.V().hasLabel('source').has('id', '{}'))".format(
                 lemma, address, token, pos, source_id)]
     for string in query:
         callback = client.submitAsync(string)
@@ -54,9 +55,12 @@ def __delete_entire_graph_database(client):
 
 
 
-def import_semeval2015_lemma()->None:
+def import_semeval2015_lemma():
     semeval_data = open_semeval2015_data()
     dbclient = connect_cosmosdb_client()
+    word_count = 0
+    lemma_count = 0
+    lemma_set = set(())
     try:
         __delete_entire_graph_database(dbclient)
         root = semeval_data.getroot()
@@ -71,15 +75,41 @@ def import_semeval2015_lemma()->None:
             for sentence in document:
                 print(sentence.attrib['id'])
                 for word in sentence:
+                    word_count += 1
                     token = word.text
                     if 'lemma' in word.attrib.keys():
                         lemma = word.attrib['lemma']
+                        lemma_set.add(lemma)
+                        lemma_count += 1
                         address = word.attrib['id']
                         pos = word.attrib['pos']
                         write_lemma(dbclient, lemma, token, address, pos, source_id)
         dbclient.close()
+        print("Word Count: {}".format(word_count))
+        print("Lemma Count: {}".format(lemma_count))
+        print("# of Unique Lemma: {}".format(len(lemma_set)))
+        # Database is missing 1 Lemma and about 100 lemma/document edges
+
     except Exception as e:
         raise Exception(e)    
     finally:
         dbclient.close()
         print("Database closed")
+
+
+
+def import_semeval2015_keys():
+    multi_word = 0
+    key_reader = open_semeval2015_keys()
+    print(key_reader)
+    for line in key_reader:
+        if line[0] != line[1]: multi_word += 1
+        print(line[0])
+        print(line[1])
+        print(line[2])
+        print(len(line))
+        for i in range(3, len(line)):
+            print(line[i])
+            
+    print("Lines: {}".format(key_reader.line_num))
+    print("Multi Word: {}".format(multi_word))
